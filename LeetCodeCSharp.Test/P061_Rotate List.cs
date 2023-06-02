181c1faa9806d159a8dc44960df2d5b82f670f84
slothman5566 <ccps960316@gmail.com>
2023-06-02 22:15:16 +0800

    feat:P061_Rotate List

--- LeetCodeCSharp.Test/P061_Rotate List.cs
+++ LeetCodeCSharp.Test/P061_Rotate List.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using LeetCodeCSharp.P061_RotateList;
+using System.Collections.Generic;
+
 namespace LeetCodeCSharp.Test
 {
     [TestClass]
-    internal class P061_Rotate_List
+    public class P061_Rotate_List
     {
 
         protected Solution _Solution = new Solution();
@@ -13,9 +15,35 @@ namespace LeetCodeCSharp.Test
         [TestMethod]
         public void TestMethod1()
         {
-
+            var result = _Solution.RotateRight(ListNode.BuildListNode(new List<int> { 1, 2, 3, 4, 5 }), 2);
             //test
-            //CollectionAssert.AreEqual(_Solution.RotateRight());
+            CollectionAssert.AreEqual(ListNode.ConvertListNodeToList(result), new List<int>() { 4, 5, 1, 2, 3 });
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = _Solution.RotateRight(ListNode.BuildListNode(new List<int> { 0, 1, 2 }), 4);
+            //test
+            CollectionAssert.AreEqual(ListNode.ConvertListNodeToList(result), new List<int>() { 2, 0, 1 });
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = _Solution.RotateRight(null, 4);
+            //test
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            ///time limit error
+            var result = _Solution.RotateRight(ListNode.BuildListNode(new List<int> { 1,2,3 }), 2000000000);
+            //test
+            CollectionAssert.AreEqual(ListNode.ConvertListNodeToList(result), new List<int>() { 2,3,1});
         }
     }
 }
--- LeetCodeCsharp/P061_RotateList.cs
+++ LeetCodeCsharp/P061_RotateList.cs
@@ -10,7 +10,30 @@ namespace LeetCodeCSharp.P061_RotateList
     {
         public ListNode RotateRight(ListNode head, int k)
         {
-            throw new NotImplementedException();
+            if (k == 0 || head == null || head.next == null)
+            {
+                return head;
+            }
+            var current = head;
+            var prev = head;
+            var count = 1;
+            while (current.next != null)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            if (k > count)
+            {
+                k = k % count;
+            }
+            if (k == 0)
+            {
+                return head;
+            }
+            prev.next = null;
+            current.next = head;
+            return RotateRight(current, --k);
         }
     }
 }
