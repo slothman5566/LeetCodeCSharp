b6d92699059c713f326e62663980d6e806a970fc
slothman5566 <ccps960316@gmail.com>
2023-06-02 21:47:55 +0800

    fix:修改測試

---
+++ LeetCodeCSharp.Test/P061_Rotate List.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LeetCodeCSharp.P061_RotateList;
+namespace LeetCodeCSharp.Test
+{
+    [TestClass]
+    internal class P061_Rotate_List
+    {
+
+        protected Solution _Solution = new Solution();
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //test
+            //CollectionAssert.AreEqual(_Solution.RotateRight());
+        }
+    }
+}
---
+++ LeetCodeCsharp/P061_RotateList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.P061_RotateList
+{
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
