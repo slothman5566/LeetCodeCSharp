3390b3f845d08d88eadde1173e3c834c3269fba7
slothman5566 <ccps960316@gmail.com>
2023-06-04 19:56:11 +0800

    feat:P623_AddOneRowtoTree

---
+++ LeetCodeCSharp.Test/P623_AddOneRowtoTree.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LeetCodeCSharp.P623_AddOneRowtoTree;
+using System.Collections.Generic;
+namespace LeetCodeCSharp.Test
+{
+    [TestClass]
+    public class P623_AddOneRowtoTree
+    {
+        protected readonly Solution _solution = new Solution();
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var root = TreeNode.BuildTreeNode(new List<int?>() { 4, 2, 6, 3, 1, 5 });
+            var result = _solution.AddOneRow(root, 1, 2);
+            var ans = TreeNode.BuildTreeNode(new List<int?>() { 4, 1, 1, 2, null, null, 6, 3, 1, null, null, null, null, 5 });
+            Assert.IsTrue(TreeNode.IsSameTree(result, root));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            var root = TreeNode.BuildTreeNode(new List<int?>() { 4, 2, null,3,1 });
+            var result = _solution.AddOneRow(root, 1, 3);
+            var ans = TreeNode.BuildTreeNode(new List<int?>() { 4, 2, null, 1, 1, null, null, 3, null, null, 1 });
+            Assert.IsTrue(TreeNode.IsSameTree(result, root));
+        }
+    }
+}
---
+++ LeetCodeCsharp/p623_AddOneRowtoTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.P623_AddOneRowtoTree
+
+{
+    public class Solution
+    {
+        public TreeNode AddOneRow(TreeNode root, int val, int depth)
+        {
+            if (root == null) return null;
+            if (depth == 1)
+            {
+                var node = new TreeNode(val);
+                node.left = root;
+                return node;
+            }
+            if (depth == 2)
+            {
+                var left = root.left;
+                root.left = new TreeNode(val);
+                root.left.left = left;
+
+                var right = root.right;
+                root.right = new TreeNode(val);
+                root.right.right = right;
+            }
+            else
+            {
+                AddOneRow(root.left, val, depth - 1);
+                AddOneRow(root.right, val, depth - 1);
+              
+            }
+            return root;
+        }
+    }
+}
